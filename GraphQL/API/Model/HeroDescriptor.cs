38764010b6b0dcf3b11b4085be6026c967a2b56f
Viwe Nkepu <viwe.nkepu@hotmail.com>
2022-06-30 20:26:58 +0200

    build errors

--- GraphQL/API/Model/HeroDescriptor.cs
+++ GraphQL/API/Model/HeroDescriptor.cs
@@ -12,7 +12,7 @@ public class HeroDescriptor : ObjectType<Hero>
         descriptor.Field(p => p.Avatar)
             .Description("The link to the only photo we have on record.");
 
-        descriptor.Field(p => p.)
-            .Description("A pseudonym to protect those they hold dear.");
+        descriptor.Field(p => p.Morality)
+            .Description("Good or Bad ? Often times the protagonist does not always end up the hero.");
     }
 }
